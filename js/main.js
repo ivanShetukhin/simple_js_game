0e7b3b5a7d9dc3bda8c0bdc99a123ded1546d09b
ivoshet <ivan.v.shet@gmail.com>
2018-02-23 20:45:47 +0500

    added many rectangle function

--- js/main.js
+++ js/main.js
@@ -11,34 +11,52 @@ var typeFeaturesList =
     {type: "rectangle"},
     {type: "straight line"},
     {type: "inclined line"},
-
+    {type: "broken line"},
 ];
+
+var sizeTextLine = 
+[
+    {size_one: 10},
+]
 
 function askTypeFeatures ()
 {
-    var promptAsk = prompt ("nl or sl or r");
+    var promptAsk = prompt ("nl or sl or r or b or m");
     globalAskVariable = promptAsk;
 }
 
 function firstMethod () {
     if (globalAskVariable == "nl")
     {
-        alert (String(typeFeaturesList[2].type));
         var canvas = document.getElementById ("canvasId");
         var ctx = canvas.getContext("2d");
+        ctx.strokeText(String(typeFeaturesList[2].type), 10, 10);
         ctx.moveTo (50, 70);
         ctx.lineTo (100, 50);
         ctx.stroke ();
     }
     if (globalAskVariable == "sl")
     {
-        alert (String(typeFeaturesList[1].type));
         secondMethod ();
     }
     if (globalAskVariable == "r")
     {
-        alert (String(typeFeaturesList[0].type));
         drawRectangle ();
+    }
+    if (globalAskVariable == 'b')
+    {
+        drawBrokenLine ();
+    }
+    if (globalAskVariable == 'm')
+    {
+        try {
+            drawManyRectangle (20, 30, 'brawn');
+            drawManyRectangle (30, 60, 'blue');
+            drawManyRectangle (40, 90, 'red');
+        }
+        catch (e) {
+            console.log (e);
+        }
     }
 }
 
@@ -46,17 +64,56 @@ function secondMethod ()
 {
     var canvas = document.getElementById ("canvasId");
     var ctx = canvas.getContext ("2d");
+    ctx.strokeText(String(typeFeaturesList[1].type), 10, 10);
     ctx.moveTo (10, 10);
     ctx.lineTo (10, 50);
     ctx.stroke ();
 }
+//draw broken line
+function drawBrokenLine () {
+    var canvas = document.getElementById ("canvasId");
+    var ctx = canvas.getContext ("2d");
+    try {
+        ctx.strokeText (String(typeFeaturesList[3].type), 10, 10);
+    }
+    catch (e) {
+        console.log (e);
+    }
+    ctx.stroke ();
+    ctx.lineWidth = 10;
+    ctx.strokeStyle = "blue";
+    var x = [40, 70, 100, 130, 160, 190];
+    var y = [40, 80, 40, 80, 40, 80];
+    ctx.moveTo (10, 80);
+    ctx.beginPath ();
+    for (var i = 0; i < x.length; i++) {
+        ctx.lineTo (x[i], y[i]);
+    }
+    ctx.stroke ();
+}
+//--------------------------------------------------------
+//--------------------------------------------------------
+function getCanvasMethod () {
+    var canvas  = document.getElementById ("canvasId");
+    var ctx = canvas.getContext ("2d");
+    return ctx;
+}
 
+function drawManyRectangle (side, x, color) {
+    var ctx = getCanvasMethod ();
+    ctx.strokeStyle = color;
+    ctx.strokeRect (x, 10, side, side);
+    ctx.stroke ();
+}
+//----------------------------------------------------------
+//----------------------------------------------------------
 //draw rectangle by canvas
 function drawRectangle () {
     var canvas  = document.getElementById ("canvasId");
     var ctx = canvas.getContext ("2d");
     canvas.width = 200;
     canvas.height = 100;
+    ctx.strokeText (String(typeFeaturesList[0].type), 10, 10);
     ctx.strokeStyle = "green";
     ctx.fillStyle = "blue";
     ctx.shadowBlur = 10;
