ee245f3ae3e1b6b076584a8aac97b13172197201
ivoshet <ivan.v.shet@gmail.com>
2018-03-14 20:22:05 +0500

    add function for guess blur image

--- js/second.js
+++ js/second.js
@@ -1,12 +1,20 @@
-function main() {
-    var one = document.write("second page");
+function addImage() {
+    var imageFromList = document.getElementById("zero");
+    if (imageFromList != null) {
+        imageFromList.onclick = showOtherImage;
+    }
 }
 
+function showOtherImage() {
+    var imageFromList = document.getElementById("zero");
+    if (imageFromList !== null) {
+        imageFromList.src = "assets/chicken.png";
+    }
+}
 
-
-
-
-
-
-
+//the input point function
+function main() {
+    addImage();
+}
+//the input point
 main();
