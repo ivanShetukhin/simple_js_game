99af37ebe8d64180ec7033cb8d3fa938240ab5b7
ivoshet <ivan.v.shet@gmail.com>
2018-03-21 23:55:05 +0700

    added circulitin js

--- js/three.js
+++ js/three.js
@@ -1,8 +1,29 @@
+function makeCounter() {
+    var count = 0;
+
+    function counter () {
+        count = count + 1;
+        return count;
+    }
+    return counter;
+}
+
+function makeCounterOne() {
+    var count = makeCounter();
+    wrD(count);
+    wrD(count);
+    wrD(count);
+    wrD(count);
+}
+
+function wrD(text) {
+    document.write(text);
+}
+
+//INPUT POINT____________________
+
 function main() {
-    document.write("three");
-
-
-
+    makeCounterOne();
 }
 
 window.onload = main;
